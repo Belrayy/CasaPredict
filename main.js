fabc752e3d8d2caea5b83ce9411a92e32e02bfcd
Belrayy <111245340+Belrayy@users.noreply.github.com>
2026-04-23 16:13:00 +0000

    Electron v2

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
+const path = require("path");
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 292,
     height: 430,
-    resizable: false,
-    maximizable: false,
+    resizable: true,
     fullscreenable: false,
-    frame: false, 
+    frame: true, 
     transparent: false,
+    icon: path.join(__dirname, "assets/images/casa_predict_logo.png"),
     webPreferences: {
       contextIsolation: true
     }
   });
 
-  win.loadFile("index.html");
+  win.loadFile(path.join(__dirname, "index.html"));
 }
 
 app.whenReady().then(createWindow);
